aaf53240236ace9f5bab4cc37c616de92d9a9a56
KevalMarmik <133678949+KevalMarmik@users.noreply.github.com>
2023-06-22 16:18:50 -0400

    Update terraformfile.tf

--- terraformfile.tf
+++ terraformfile.tf
@@ -15,6 +15,7 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
+
 resource "aws_security_group" "myec2" {
   name        = "myec2-security-group"
   ingress {
@@ -24,6 +25,7 @@ resource "aws_security_group" "myec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_instance" "myec2" {
    ami = "ami-0bef6cc322bfff646"
    instance_type = "t2.micro"
